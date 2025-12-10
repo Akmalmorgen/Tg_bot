e0a534c6972647a469a135864fc14698de8d13a2
Gojo <148787541+Akmalmorgen@users.noreply.github.com>
2025-12-10 23:27:28 +0500

    Delete who_anonim_bot directory

--- who_anonim_bot/config/init.py
+++
@@ -1,2 +0,0 @@
-# who_anonim_bot/config/__init__.py
-# package
--- who_anonim_bot/logger/logger.py
+++
@@ -1,9 +0,0 @@
-# who_anonim_bot/logger/logger.py
-import logging
-
-def setup_logger():
-    logging.basicConfig(
-        level=logging.INFO,
-        format="%(asctime)s [%(levelname)s] %(message)s"
-    )
-    logging.getLogger("aiosqlite").setLevel(logging.WARNING)
