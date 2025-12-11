ccc9f365b542fe3eee4b38b49279f29b3847c826
Gojo <148787541+Akmalmorgen@users.noreply.github.com>
2025-12-11 15:21:06 +0500

    Update settings.py

--- who_anonim_bot/config/settings.py
+++ who_anonim_bot/config/settings.py
@@ -1,13 +1,15 @@
-import os
+# === TOKEN ===
+# Твой токен бота (обязательно!)
+TOKEN = "8134649307:AAH3k13igSnG9t6lGYYTvu7e2D4RiKaCXMI"
 
-# Токен бота
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8134649307:AAH3k13igSnG9t6lGYYTvu7e2D4RiKaCXMI")
+# === USERNAME БОТА ===
+BOT_USERNAME = "Who_Anonim_Bot"
 
-# Юзернейм бота (без @)
-BOT_USERNAME = os.getenv("BOT_USERNAME", "Who_Anonim_Bot")
+# === АДМИНЫ ===
+# Можно несколько
+ADMINS = [
+    7967404620,  # твой ID
+]
 
-# Админы (ID через запятую)
-ADMINS = [7967404620]
-
-# Директория базы данных
-DB_PATH = "who_anonim_bot/database.db"
+# === БАЗА ДАННЫХ ===
+DATABASE_PATH = "who_anonim_bot/WhoAnonim.db"
