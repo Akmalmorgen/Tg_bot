c962ba5351c0fc8d294ba803ed3d2f6c21d88ea5
Gojo <148787541+Akmalmorgen@users.noreply.github.com>
2025-12-11 14:40:49 +0500

    Create __init__.py

---
+++ who_anonim_bot/handlers/__init__.py
@@ -0,0 +1,20 @@
+from .start import register_start_handlers
+from .menu import register_menu_handlers
+from .anon_link import register_anon_link_handlers
+from .anon_chat import register_anon_chat_handlers
+from .roulette import register_roulette_handlers
+from .admin import register_admin_handlers
+
+
+def register_all_handlers(application):
+    """
+    Регистрирует все обработчики в боте.
+    Вызывается из main.py
+    """
+
+    register_start_handlers(application)
+    register_menu_handlers(application)
+    register_anon_link_handlers(application)
+    register_anon_chat_handlers(application)
+    register_roulette_handlers(application)
+    register_admin_handlers(application)
