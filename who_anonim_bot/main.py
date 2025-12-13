a2dca4ed2a9b4f21cf90fae88eee0d3e6ea248e5
Gojo <148787541+Akmalmorgen@users.noreply.github.com>
2025-12-13 23:05:30 +0500

    Update main.py

--- who_anonim_bot/main.py
+++ who_anonim_bot/main.py
@@ -1,48 +1,28 @@
 import asyncio
-from telegram.ext import (
-    Application,
-    MessageHandler,
-    CommandHandler,
-    CallbackQueryHandler,
-    filters
-)
 
-# Импортируем настройки
+from telegram.ext import Application
+
 from config.settings import TOKEN
-
-# Импортируем регистрацию всех хендлеров
-from handlers import (
-    register_start_handlers,
-    register_menu_handlers,
-    register_anon_link_handlers,
-    register_anon_chat_handlers,
-    register_roulette_handlers,
-    register_admin_handlers,
-    register_broadcast_handlers
-)
-
-# Подключение базы
+from logger.logger import setup_logger
 from db.database import init_db
+from handlers import register_all_handlers
 
 
 async def main():
-    # Инициализация бота
-    app = Application.builder().token(TOKEN).build()
+    # логирование
+    setup_logger()
 
-    print("📦 Инициализация базы данных...")
+    # база данных
     await init_db()
 
-    print("🔗 Регистрация обработчиков...")
-    register_start_handlers(app)
-    register_menu_handlers(app)
-    register_anon_link_handlers(app)
-    register_anon_chat_handlers(app)
-    register_roulette_handlers(app)
-    register_admin_handlers(app)
-    register_broadcast_handlers(app)
+    # приложение
+    application = Application.builder().token(TOKEN).build()
 
-    print("🚀 Бот запущен и слушает обновления!")
-    await app.run_polling()
+    # регистрация всех обработчиков
+    register_all_handlers(application)
+
+    # запуск
+    await application.run_polling()
 
 
 if __name__ == "__main__":
