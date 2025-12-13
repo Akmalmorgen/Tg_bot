0ddbf4b930891159642198892e1007fea6bbcf1e
Gojo <148787541+Akmalmorgen@users.noreply.github.com>
2025-12-13 23:19:26 +0500

    Update logger.py

--- who_anonim_bot/logger/logger.py
+++ who_anonim_bot/logger/logger.py
@@ -1,21 +1,18 @@
 import logging
 import sys
 
-def setup_logger():
-    # Создаём логгер
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
 
-    # Формат логов
-    formatter = logging.Formatter(
-        fmt="[{asctime}] [{levelname}] {message}",
-        style="{",
-        datefmt="%Y-%m-%d %H:%M:%S",
+def setup_logger():
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+        handlers=[
+            logging.StreamHandler(sys.stdout)
+        ]
     )
 
-    # Вывод в консоль (важно для GSM Hosting)
-    console = logging.StreamHandler(sys.stdout)
-    console.setFormatter(formatter)
-    logger.addHandler(console)
+    # уменьшаем спам от telegram
+    logging.getLogger("httpx").setLevel(logging.WARNING)
+    logging.getLogger("telegram").setLevel(logging.INFO)
 
-    return logger
+    logging.info("✅ Logger инициализирован")
